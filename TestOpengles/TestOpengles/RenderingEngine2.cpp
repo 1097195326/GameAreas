f0427671d8a5b89d97353883de3628c1f184494a
1097195326 <1097195326@qq.com>
2015-11-25 18:08:01 +0800

    2015.11.25 opengles 01

--- TestOpengles/TestOpengles/RenderingEngine2.cpp
+++ TestOpengles/TestOpengles/RenderingEngine2.cpp
@@ -7,7 +7,10 @@
 //
 
 #include "IRenderingEngine.h"
+#include <iostream>
 #include <OpenGLES/ES2/gl.h>
+//#include <OpenGLES/ES2/glext.h>
+
 
 class RenderingEngine2 : public IRenderingEngine
 {
@@ -45,4 +48,48 @@ void RenderingEngine2::render()const
 void RenderingEngine2::updateAnimation(float timeStep)
 {
     
+}
+GLuint RenderingEngine2::buildShader(const char *source, GLenum type) const
+{
+    GLuint shaderHandler = glCreateShader(type);
+    glShaderSource(shaderHandler, 1, &source, 0);
+    glCompileShader(shaderHandler);
+    
+    GLint compileSuccess;
+    glGetShaderiv(shaderHandler, GL_COMPILE_STATUS, &compileSuccess);
+    
+    if (compileSuccess == GL_FALSE)
+    {
+        GLchar message[256];
+        glGetShaderInfoLog(shaderHandler, sizeof(message), 0, &message[0]);
+        std::cout<<message;
+        exit(1);
+    }
+    
+    return shaderHandler;
+}
+GLuint RenderingEngine2::buildProgram(const char *vShader, const char *fShader) const
+{
+    GLuint vertexShader = buildShader(vShader, GL_VERTEX_SHADER);
+    GLuint fragmentShader = buildShader(fShader, GL_FRAGMENT_SHADER);
+
+    GLuint programHander = glCreateProgram();
+    glAttachShader(programHander, vertexShader);
+    glAttachShader(programHander, fragmentShader);
+    
+    glLinkProgram(programHander);
+    
+    GLint programSuccess;
+    glGetProgramiv(programHander, GL_LINK_STATUS, &programSuccess);
+    
+    if (programSuccess == GL_FALSE)
+    {
+        GLchar message[256];
+        glGetProgramInfoLog(programHander, sizeof(message), 0, &message[0]);
+        std::cout<<message;
+        
+        exit(1);
+    }
+    
+    return programHander;
 }
